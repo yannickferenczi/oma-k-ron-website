b7aa34980059f8fbd1af8c737d16abf50b15dd24
yannickferenczi <>
2023-10-17 19:56:13 +0000

    Add cart modification functionalities

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 
 from products.models import Product
@@ -8,7 +8,10 @@ def display_cart(request):
     """
     A view to display the cart content
     """
-    return render(request, "cart/cart.html")
+    context = {
+        "on_cart_page": True,
+    }
+    return render(request, "cart/cart.html", context)
 
 
 def add_to_cart(request, item_id):
@@ -35,3 +38,25 @@ def add_to_cart(request, item_id):
         )
     request.session["cart"] = cart
     return redirect(redirect_url)
+
+
+def adjust_cart(request, item_id):
+    product = get_object_or_404(Product, pk=item_id)
+    quantity = int(request.POST.get("quantity"))
+    cart = request.session.get("cart", {})
+    if quantity > 0:
+        cart[item_id] = quantity
+        messages.success(
+            request,
+            f"The quantity of {product.name} has been successfully \
+                updated to {cart[item_id]}.",
+        )
+    else:
+        cart.pop(item_id)
+        messages.success(
+            request,
+            f"{product.name} has been successfully \
+                removed from your shopping cart.",
+            )
+    request.session["cart"] = cart
+    return redirect(reverse("cart"))
