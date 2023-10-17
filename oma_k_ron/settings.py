8dd66789dc4e7dbdc4e02f4cbb2895c666e8b561
yannickferenczi <>
2023-10-17 08:45:26 +0000

    Fix sending emails

--- oma_k_ron/settings.py
+++ oma_k_ron/settings.py
@@ -229,7 +229,7 @@ else:
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-    DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
 
 # CountryField
