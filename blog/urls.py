763a1bcd55da37ef0cc35ac1f1496f965360a7fc
yannickferenczi <>
2023-10-21 19:58:14 +0000

    Add PostDetail view and template

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.PostList.as_view(), name="post_list"),
+    path("post_detail/<slug>/", views.PostDetail.as_view(), name="post_detail"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 
 from .models import Post
 
@@ -8,3 +8,10 @@ class PostList(generic.ListView):
     model = Post
     queryset = Post.objects.filter(published=True)
     template_name = "blog/post_list.html"
+
+
+class PostDetail(View):
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(published=True)
+        post = get_object_or_404(queryset, slug=slug)
+        return render(request, "blog/post_detail.html", {"post": post})
