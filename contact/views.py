42a9b407c0b857756c6f7d72e04a4bb88a83208d
yannickferenczi <>
2023-10-23 18:36:52 +0000

    Optimize email confirmations for orders and contact form

--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,11 @@
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponse
 from django.contrib import messages
+from django.template.loader import render_to_string
+from django.core.mail import send_mail
+from django.conf import settings
 
 from .forms import ContactForm
 
@@ -11,9 +16,28 @@ def contact(request):
         form = ContactForm(request.POST)
         if form.is_valid():
             form.save()
+            full_name = form.cleaned_data["full_name"]
+            email = form.cleaned_data["email"]
+            request_subject = form.cleaned_data["subject"]
+            message = form.cleaned_data["message"]
+            subject = "Thank you for your email to Oma K-ron"
+            body = render_to_string(
+                "contact/email/email_request_received.txt",
+                {
+                    "full_name": full_name,
+                    "request_subject": request_subject,
+                    "message": message,
+                },
+            )
+            send_mail(
+                subject,
+                body,
+                settings.DEFAULT_FROM_EMAIL,
+                [email],
+            )
             messages.success(
                 request,
-                "Your form has been successfully submitted!",
+                "Your request has been successfully submitted!",
             )
             return redirect(reverse("products"))
         else:
